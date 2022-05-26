97732f7ff7e1864653446b1924295ce89a0d362d
Rami Richani <65448126+dolf321@users.noreply.github.com>
2022-05-27 02:17:21 +0400

    Added depthai implementations to cmake and new ros node that conencts depth ai of oak d camera and orb slam2  and then outputs tf2 position as word frame

--- build_ros.sh
+++ build_ros.sh
@@ -3,5 +3,6 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM2
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
+#cmake .. -DROS_BUILD_TYPE=Release -D'depthai-core_DIR=/home/rami/nr22-software/include/depthai-core-old/build/lib/cmake/depthai-core'
+cmake .. -DROS_BUILD_TYPE=Release -D'depthai_DIR=/home/rami/nr22-software/include/depthai-core/build'
 make -j
